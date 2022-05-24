167ab867a9f66144beb456617e1231430caf9e9f
Alfred Leandro Liang <s10223157@connect.np.edu.sg>
2022-05-24 10:03:38 +0800

    Just implemented the basic feature of the Hamburger + the search bar. Pushing file just to test if I can push code without messing up anything. Left with displaying grid layout.
    P.S. the Hamburger Menu works but the buttons don't do anything (for now) and the searchBar also doesn't do anything yet.

--- app/src/main/java/sg/edu/np/mad/devent/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/devent/MainActivity.java
@@ -1,14 +1,28 @@
 package sg.edu.np.mad.devent;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.ui.AppBarConfiguration;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.navBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent navDraw = new Intent(MainActivity.this, NavDrawer.class);
+                startActivity(navDraw);
+            }
+        });
     }
+
 }
