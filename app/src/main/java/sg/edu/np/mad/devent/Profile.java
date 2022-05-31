9e414377f8db254a0f0a8479d575988af155fc7e
Ngui Jia Le Sherlena <Sherlena2002@gmail.com>
2022-05-31 10:37:11 +0800

    Added the hashing of password

--- app/src/main/java/sg/edu/np/mad/devent/Profile.java
+++ app/src/main/java/sg/edu/np/mad/devent/Profile.java
@@ -1,5 +1,7 @@
 package sg.edu.np.mad.devent;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 public class Profile {
@@ -9,11 +11,13 @@ public class Profile {
     int Contactnum;
     int Eventsattended;
     int Saltvalue;
+    String Saltpassword;
+    String Hashedpassword;
 
 
     public Profile() {}
 
-    public Profile(String username, String title, Integer contact, int id){
+    public Profile(String username, String title, Integer contact, String password, int id){
         Id = id;
         Username = username;
         Title = title;
@@ -22,6 +26,40 @@ public class Profile {
         int lowerbound = 10000;
         int upperbound = 99999;
         int randomnum = rand.nextInt(upperbound);
-        //Uncomment this later Saltvalue =
+        while (randomnum < lowerbound){
+            randomnum = rand.nextInt(upperbound);
+        }
+        Saltvalue = randomnum;
+        Saltpassword = Saltvalue + password;
+        try
+        {
+            /* MessageDigest instance for MD5. */
+            MessageDigest m = MessageDigest.getInstance("MD5");
+
+            /* Add plain-text password bytes to digest using MD5 update() method. */
+            m.update(Saltpassword.getBytes());
+
+            /* Convert the hash value into bytes */
+            byte[] bytes = m.digest();
+
+            /* The bytes array has bytes in decimal form. Converting it into hexadecimal format. */
+            StringBuilder s = new StringBuilder();
+            for (int i=0; i< bytes.length ;i++)
+            {
+                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+            /* Complete hashed password in hexadecimal format */
+            Hashedpassword = s.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+
+        /* Display the unencrypted and encrypted passwords. */
+        System.out.println("Plain-text password: " + password);
+        System.out.println("Encrypted password using MD5: " + Hashedpassword);
     }
+
 }
