5cc2d647f0450a8fa110b4a9505cbab3be33eee9
s10221930@connect.np.edu.sg <s10221930@connect.np.edu.sg>
2022-06-02 12:37:35 +0800

    Uploaded and Connected Firebase Database to the project.

--- app/src/main/java/sg/edu/np/mad/devent/EventDetailsPage.java
+++ app/src/main/java/sg/edu/np/mad/devent/EventDetailsPage.java
@@ -1,11 +1,18 @@
 package sg.edu.np.mad.devent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -15,6 +22,26 @@ public class EventDetailsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details_page);
+
+        //For firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://dvent---ducktectives-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        DatabaseReference Ref = database.getReference("Event/Event1");
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Integer eventID = (Integer) dataSnapshot.child("EventID").getValue();
+                Integer price = (Integer) dataSnapshot.child("Pricing").getValue();
+                Log.d("aa", price + " / " + eventID);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("Error","An error occured!"); //Don't ignore errors!
+            }
+
+        };
+        Ref.addListenerForSingleValueEvent(valueEventListener);
 
         imageSlider =findViewById(R.id.imageslider);
 
