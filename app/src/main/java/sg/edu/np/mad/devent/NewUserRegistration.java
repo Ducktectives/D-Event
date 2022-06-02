d9de4e566002568f5cf21eb245e9bc1533e9d25e
Ngui Jia Le Sherlena <Sherlena2002@gmail.com>
2022-06-02 17:49:16 +0800

    Addition of new user registration page

---
+++ app/src/main/java/sg/edu/np/mad/devent/NewUserRegistration.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.devent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewUserRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user_registration);
+    }
+}
