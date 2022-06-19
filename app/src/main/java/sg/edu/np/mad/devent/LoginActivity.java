658baede0bab748ac662931532c05b304e62f520
Ngui Jia Le Sherlena <Sherlena2002@gmail.com>
2022-06-19 21:42:00 +0800

    Edit to the input validation of Registration

--- app/src/main/java/sg/edu/np/mad/devent/LoginActivity.java
+++ app/src/main/java/sg/edu/np/mad/devent/LoginActivity.java
@@ -3,6 +3,8 @@ package sg.edu.np.mad.devent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,6 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText email = (EditText)findViewById(R.id.loginuseremail);
+        EditText password = (EditText)findViewById(R.id.loginuserpassword);
 
 
 
