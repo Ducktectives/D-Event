cb1f3e1ea6fdfbc1a7af6b8e0398e0aca092f52d
Ngui Jia Le Sherlena <Sherlena2002@gmail.com>
2022-06-21 01:04:49 +0800

    Inserted a splash screen

--- app/src/main/java/sg/edu/np/mad/devent/SplashScreen.java
+++ app/src/main/java/sg/edu/np/mad/devent/SplashScreen.java
@@ -2,7 +2,9 @@ package sg.edu.np.mad.devent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ProgressBar progressbar = (ProgressBar)findViewById(R.id.progressBar);
+
+        progressbar.setProgress(25);
+
+        progressbar.setProgress(50);
+
+        /*
+        if (){
+            progressbar.setProgress(75);
+            Intent i1 = new Intent(SplashScreen.this, loginpage.class);
+            progressbar.setProgress(100);
+            startActivity(i1);
+        }
+        else {
+            progressbar.setProgress(75);
+            Intent i1 = new Intent(SplashScreen.this, loginpage.class);
+            progressbar.setProgress(100);
+            startActivity(i1);
+        }
+        */
     }
 }
